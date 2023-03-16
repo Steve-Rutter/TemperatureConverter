d75dd25192f43555d14bd57689445b9a99957d8d
Steve Rutter <steve.rutter@towerswatson.com>
2023-03-16 20:14:30 +0000

    First commit

---
+++ TemperatureModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TempConverter
+{
+    public class TemperatureModel
+    {
+        [Required]
+        public double TemperatureValue { get; set; }
+        [Required]
+        public string? SourceTemperatureType { get; set; }
+        [Required]
+        public string? TargetTemperatureType { get; set; }
+    }
+}
